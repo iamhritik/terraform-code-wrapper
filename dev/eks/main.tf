af9c1576cc85a5970b78a1ca41c387beb1b31075
iamhritik <hritik.99@outlook.com>
2023-07-10 00:30:13 +0530

    correct depends_on

--- dev/eks/main.tf
+++ dev/eks/main.tf
@@ -57,7 +57,7 @@ data "aws_security_group" "controlplane_sg" {
 }
 
 #Jenkins SG to communicate with eks cluster
-resource "aws_security_group_rule" "example" {
+resource "aws_security_group_rule" "jenkins_add" {
   security_group_id        = data.aws_security_group.controlplane_sg.id
   type                     = "ingress"
   from_port                = 443
@@ -66,8 +66,7 @@ resource "aws_security_group_rule" "example" {
   source_security_group_id = "sg-001d4d01d818ed07f" #specify SG ID that you used to connect with kubernetes API server
   depends_on = [
     module.dev_eks_cluster,
-    data.aws_security_group.controlplane_sg,
-    kubectl_manifest.karpenter_node_template
+    data.aws_security_group.controlplane_sg
   ]
 }
 
