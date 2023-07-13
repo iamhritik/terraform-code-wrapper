b39501e84a2c68ed1e9bdcdfa3eac5906af4111e
iamhritik <hritik.99@outlook.com>
2023-07-14 02:00:29 +0530

    update aws-auth cm

--- dev/eks/aws_auth.tf
+++ dev/eks/aws_auth.tf
@@ -2,12 +2,16 @@ locals {
   aws_auth_roles = concat(
     [
       {
-        rolearn  = module.karpenter.role_name
+        rolearn  = module.dev_eks_cluster.node_iam_role_arn
         username = "system:node:{{EC2PrivateDNSName}}"
         groups   = ["system:bootstrappers", "system:nodes"]
       },
-    ],
-    var.eks_roles
+      {
+        rolearn  = module.karpenter.role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ]
   )
 }
 
@@ -24,7 +28,7 @@ data:
   mapAccounts: |
     []
   mapRoles: |
-    ${indent(4, yamlencode(local.aws_auth_roles))}
+${indent(4, yamlencode(local.aws_auth_roles))}
   mapUsers: |
     []
 YAML
